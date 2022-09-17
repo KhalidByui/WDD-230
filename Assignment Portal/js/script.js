3bf12cd27f9ae215c138478251c8790c06aecac3
KhalidByui <u243013844@byui.edu>
2022-09-17 01:08:48 -0600

    Assignment Portal
    Created assignment portal

---
+++ Assignment Portal/js/script.js
@@ -0,0 +1,5 @@
+const year = new Date().getFullYear();
+document.querySelector(".year").innerHTML = `© ${year} || Elerunndu Victor Oluwapelumi || WDD 230`
+
+
+document.querySelector(".updated").innerHTML = `Last Updated: ${document.lastModified}`;
