c0f3a4079a1b6791f6e83ff01c2064c97d8c1b83
Elerunndu Victor Oluwapelumi <113521008+KhalidByui@users.noreply.github.com>
2022-09-17 09:04:08 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const year = new Date().getFullYear();
-document.querySelector(".year").innerHTML = `© ${year} || Elerunndu Victor Oluwapelumi || WDD 230`
+document.querySelector(".year").innerHTML = `© ${year} .:|:. Elerunndu Victor Oluwapelumi .:|:. Nigeria`
 
 
 document.querySelector(".updated").innerHTML = `Last Updated: ${document.lastModified}`;
