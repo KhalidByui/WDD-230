feaa999acc63ba68686d2067601defe7e2eff97d
KhalidByui <u243013844@byui.edu>
2022-09-23 17:29:09 -0600

    add lesson2 files

---
+++ lesson2/scripts/script.js
@@ -0,0 +1,5 @@
+const year = new Date().getFullYear();
+
+
+// to get last modiified
+document.querySelector(".year").innerHTML = `© ${year} | Elerunndu Victor Oluwapelumi | Last Updated: ${document.lastModified}`;
