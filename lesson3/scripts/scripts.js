aefd969e1e8ae7b452d9f3fd23a0164e4514b688
KhalidByui <u243013844@byui.edu>
2022-09-27 05:28:09 -0600

    created site plan document

---
+++ lesson3/scripts/scripts.js
@@ -0,0 +1,6 @@
+const year = new Date().getFullYear();
+
+
+// to get last modiified
+
+document.querySelector(".year").innerHTML = `© ${year} | Elerunndu Victor Oluwapelumi | Last Updated: ${document.lastModified}`;
